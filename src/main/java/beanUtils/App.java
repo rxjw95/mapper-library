8dacc131a696e8328264582a9969b22623d73117
Jangwook Ryu <rxjw95@gmail.com>
2022-06-25 22:34:14 +0900

    [BeanUtils]
    - 테스트 코드로 배우는 BeanUtils

--- src/main/java/beanUtils/App.java
+++ src/main/java/beanUtils/App.java
@@ -1,12 +1,7 @@
 package beanUtils;
 
-import java.util.List;
-
 public class App {
     public static void main(String[] args) {
-        User sourceUser = new User("jangwook", 28, List.of("football", "tennis"));
-        User targetUser = new User();
-
-        System.out.println(targetUser);
+        // test code 참고
     }
 }
